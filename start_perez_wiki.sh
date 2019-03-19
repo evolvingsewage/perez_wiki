cea5474e0c6e8725c54a94707c8b34a9e8699c8b
cperez941 <carlp941@gmail.com>
2019-03-18 22:01:49 -0500

    Created gunicorn infrastructure

---
+++ start_perez_wiki.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Author: Carl Perez
+# Description: Starts the Perez Wiki Website with gunicorn
+
+usage() {
+    echo -e "Usage: ./{0} [-d]"
+    echo -e "  -d       Development Mode - use this to avoid using gunicorn"
+    echo -e "               DO NOT DO THIS IN PRODUCTION ENVIRONMENTS"
+}
+
+MODE="production"
+# let's parse args!
+while getopts ":d" o; do
+    case "${o}" in
+        d)
+            MODE="production"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Start the website in the specified mode
+# Globals:
+#   MODE
+# Arguments:
+#   None
+# Returns:
+#   None
+start_perez() {
+    source venv/bin/activate
+    export FLASK_ENV="${MODE}"
+    export FLASK_APP="perez"
+    gunicorn -w 4 --bind 0.0.0.0:8000 "perez:create_app()"
+}
+
+start_perez
