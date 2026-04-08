0b667c6f80bc1be6211a9b4cf554053fd707fa96
evolvingsewage <carlp941@gmail.com>
2026-04-08 00:35:17 -0600

    move de button to footer

--- perez/de.py
+++ perez/de.py
@@ -2,22 +2,22 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("de", __name__, url_prefix="/de")
 
-@bp.route('/')
+@bp.route('/', strict_slashes=False)
 def index():
     return render_template("de/index.html")
 
-@bp.route('/about')
+@bp.route('/about', strict_slashes=False)
 def about():
     return render_template("de/about.html")
 
-@bp.route('/resume')
+@bp.route('/resume', strict_slashes=False)
 def resume():
     return render_template("de/resume.html")
 
-@bp.route('/projects')
+@bp.route('/projects', strict_slashes=False)
 def projects():
     return render_template("de/projects.html")
 
-@bp.route('/contact')
+@bp.route('/contact', strict_slashes=False)
 def contact():
     return render_template("de/contact.html")
